1f21ea5a77a0848dc8b34de5635b1988f0a22708
Adiel <joseadielsantosguzman@gmail.com>
2024-04-22 21:37:52 -0400

    first commit

---
+++ app/src/main/java/com/optic/uberclonekotlin/activities/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.optic.uberclonekotlin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.optic.uberclonekotlin.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
